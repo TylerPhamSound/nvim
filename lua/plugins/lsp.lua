d521b37e2a886b3f1f6684194a0c1af3c1b21ae3
TylerPhamSound <tylerphamsound@gmail.com>
2025-03-15 03:48:08 -0700

    Lots of lsp shenanigans

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,46 @@
 return {
   {
+    "williamboman/mason.nvim",
+    opts = {
+      function() require('mason').setup() end,
+    }
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      function()
+        require('mason-lspconfig').setup({
+          ensure_installed = { "lua_ls", "gdscript", "csharp_ls" }
+        })
+      end,
+      function()
+        require("mason-lspconfig").setup_handlers {
+          -- The first entry (without a key) will be the default handler
+          -- and will be called for each installed server that doesn't have
+          -- a dedicated handler.
+          function(server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup {}
+          end,
+          -- Next, you can provide a dedicated handler for specific servers.
+          -- For example, a handler override for the `rust_analyzer`:
+          ["rust_analyzer"] = function()
+            require("rust-tools").setup {}
+          end,
+        }
+      end,
+    }
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
-      require("lspconfig").lua_ls.setup {}  
+      -- require 'lspconfig'.lua_ls.setup {}
+      -- require 'lspconfig'.csharp_ls.setup {}
+      -- require 'lspconfig'.gdscript.setup {}
+      -- require 'lspconfig'.gdshader_lsp.setup {}
+      local wk = require('which-key')
+      wk.add({
+        { "<leader>f", function() vim.lsp.buf.format() end, desc = "Format Current Buffer" }
+      })
     end,
-  }
+  },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,36 @@
 return {
-	{ 
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		},
-	}
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+      require("telescope").setup {
+        pickers = {
+          find_files = {
+            theme = "ivy"
+          },
+          lsp_references = {
+            theme = "dropdown"
+          }
+        }
+     }
+      local wk = require("which-key")
+      wk.add({
+        { "<leader>t",  group = "Telescope" },
+        { "<leader>tf", require("telescope.builtin").find_files,     desc = "Find files",          mode = "n" },
+        { "<leader>tr", require("telescope.builtin").lsp_references, desc = "List LSP references", mode = "n" },
+        { "<leader>e",  group = "Edit" },
+        {
+          "<leader>en",
+          function()
+            require("telescope.builtin").find_files {
+              cwd = vim.fn.stdpath("config") }
+          end,
+          desc = "Edit neovim config"
+        }
+      })
+    end
+  }
 }
