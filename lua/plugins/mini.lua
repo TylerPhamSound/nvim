d29620d11e749a85ff23f7a8d068b2de2df9a287
TylerPhamSound <tylerphamsound@gmail.com>
2025-03-28 13:53:04 -0700

    Added mini.nvim modules, split colorscheme and which-key plugins to separate files.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,10 +1,22 @@
 return {
-	{
-		"echasnovski/mini.nvim",
-		version = false,
-		config = function()
-			local statusline = require 'mini.statusline'
-			statusline.setup { use_icons = true }
-		end
-	},
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+      require('mini.ai').setup()
+      require('mini.align').setup()
+      require('mini.animate').setup()
+      require('mini.diff').setup({
+        view = {
+          style = 'number',
+          signs = { add = '+', change = '~', delete = '-' },
+        },
+      })
+      require('mini.icons').setup()
+      require('mini.indentscope').setup()
+      require('mini.operators').setup()
+      require('mini.statusline').setup({ use_icons = true })
+      require('mini.surround').setup()
+    end
+  },
 }
